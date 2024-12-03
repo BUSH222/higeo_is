00e6ad9fc83b7f95cdcd799da5489b7bd10a4561
Ted <58144503+BUSH222@users.noreply.github.com>
2024-12-03 12:20:29 +0300

    about page half

--- main.py
+++ main.py
@@ -15,4 +15,6 @@ def about():
 
 
 if __name__ == '__main__':
+    app.jinja_env.auto_reload = True
+    app.config['TEMPLATES_AUTO_RELOAD'] = True
     app.run(host='localhost', port=5000, debug=True)
