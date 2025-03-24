202124808030a290b9a503573d04806215ffeaf1
Ted <58144503+BUSH222@users.noreply.github.com>
2025-03-24 12:25:22 +0300

    removed populate

--- helper/__main__.py
+++ helper/__main__.py
@@ -1,21 +1,9 @@
-from .db.populate import populate
 from .db.initialise_database import create_tables, create_database
 
 if input("Creating database, y to continue: ") == 'y':
     create_database()
-else:
-    print('Aborting')
-
 
 if input("Creating tables, y to continue: ") == 'y':
     create_tables()
-else:
-    print('Aborting')
 
-
-if input("Populating the table with dummy data, y to continue: ") == 'y':
-    populate()
-else:
-    print('Aborting')
-
-print('Done!')
+print('Done creating tables!')
